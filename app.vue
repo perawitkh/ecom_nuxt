151b39121f0991de2813394ecb92aea153903d75
perawitkh <91835976+perawitkh@users.noreply.github.com>
2024-06-02 02:41:37 +0700

    Update app.vue
    update:h1 text changed

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">Hello</h1>
+    <h1 class="text-3xl font-bold underline">Hello Ei firsttt!!</h1>
     <button
       class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
     >
